aca8c4ab230aeaae07b3272e9d53bdd81fb4e2ce
sparklyballs <sparklyballs@gmail.com>
2017-03-05 07:17:00 +0000

    first save up xenial root builder for armxx

---
+++ scripts/tar_fetch_arm64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ curl -o \
+ ${WORKSPACE}/ubuntu-xenial-core-cloudimg-arm64-root.tar.gz -L \
+	https://partner-images.canonical.com/core/xenial/current/ubuntu-xenial-core-cloudimg-arm64-root.tar.gz
