d75f9e7532d109012168a28c2261d35c3004a9f2
sparklyballs <sparklyballs@gmail.com>
2017-04-20 11:28:46 +0100

    build xenial x86 added

---
+++ scripts/tar_fetch_x86.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ curl -o \
+ ${WORKSPACE}/https://partner-images.canonical.com/core/xenial/current/ubuntu-xenial-core-cloudimg-amd64-root.tar.gz -L \
+	https://partner-images.canonical.com/core/xenial/current/ubuntu-xenial-core-cloudimg-amd64-root.tar.gz
