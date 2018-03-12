7f1fd64a7d51f6e45189b0473f36d0bf8d3c55e3
sparklyballs <sparklyballs@gmail.com>
2018-03-12 10:19:06 +0000

    Update tar_fetch_arm64.sh

--- scripts/tar_fetch_arm64.sh
+++ scripts/tar_fetch_arm64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  curl -o \
- ${WORKSPACE}/ubuntu-xenial-core-cloudimg-root.tar.gz -L \
-	https://partner-images.canonical.com/core/xenial/current/ubuntu-xenial-core-cloudimg-arm64-root.tar.gz
+ ${WORKSPACE}/ubuntu-bionic-core-cloudimg-root.tar.gz -L \
+	https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-amd64-root.tar.gz
